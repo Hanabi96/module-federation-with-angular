8d06a11820b13a03ba8d97e24d3b71673dc82159
hanabi96 <cristian.burgos.dev@gmail.com>
2023-04-18 08:42:10 -0400

    Added product page

--- monorepo/projects/mf-shopping/webpack.config.js
+++ monorepo/projects/mf-shopping/webpack.config.js
@@ -1,15 +1,15 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
-  name: 'mf-shopping',
+  name: 'mfShopping',
 
   exposes: {
-    './Component': './projects/mf-shopping/src/app/app.component.ts',
+    './ProductsModule': './projects/mf-shopping/src/app/products/products.module.ts',
   },
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
 
+  sharedMappings: ["@commons-lib"],
 });
