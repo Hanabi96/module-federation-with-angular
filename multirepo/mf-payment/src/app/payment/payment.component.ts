11d98b9f41355366ddbbb7499d137042647fb856
hanabi96 <cristian.burgos.dev@gmail.com>
2023-04-19 10:54:28 -0400

    Module federation is configured in payment

--- multirepo/mf-payment/src/app/payment/payment.component.ts
+++ multirepo/mf-payment/src/app/payment/payment.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ICommonProduct } from '@commons-lib';
+
+export interface ICommonProduct {
+  price: number;
+  name: string;
+}
+
 
 @Component({
   standalone: true,
